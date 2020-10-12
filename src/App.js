9fd022b00888a4c2076ffec184e07f1ed44822cf
sajish <sajish.sasidharan@tarento.com>
2020-10-12 22:14:48 +0530

    added create react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="App-header">
+       <a href="/">Cart Application</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        All right is reserved
+      </footer>
+    </div>
+  ); 
+}
+
+export default App;
